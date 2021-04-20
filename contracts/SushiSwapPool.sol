66d902a9071de457ea54188f271f85769612b3af
Alain Papazoglou <alain@kredeum.com>
2021-04-20 17:31:28 +0200

    feat(sushiswappool): pool creation and tests (wip)

--- contracts/SushiSwapPool.sol
+++ contracts/SushiSwapPool.sol
@@ -1,38 +1,21 @@
-// SPDX-License-Identifier: Apache-2.0
-pragma solidity ^0.6.6;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.3;
 
 import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol";
-import "@uniswap/v2-periphery/contracts/libraries/UniswapV2Library.sol";
-import "@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol";
-// import "./UbiquityAlgorithmicDollarManager.sol";
-import "./interfaces/ISushiSwapPool.sol";
 
-contract SushiSwapPool is ISushiSwapPool {
-    address public factory = 0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f;
+contract SushiSwapPool {
+    IUniswapV2Factory public iFactory =
+        IUniswapV2Factory(0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f);
 
     address public manager;
+    address public pool;
 
-    // solhint-disable-next-line no-empty-blocks
-    constructor(address _manager) public {
+    constructor(
+        address _manager,
+        address tokenA,
+        address tokenB
+    ) {
         manager = _manager;
-    }
-
-    function pairInfo(address tokenA, address tokenB)
-        external
-        view
-        override
-        returns (
-            uint256 reserveA,
-            uint256 reserveB,
-            uint256 totalSupply
-        )
-    {
-        IUniswapV2Pair pair =
-            IUniswapV2Pair(UniswapV2Library.pairFor(factory, tokenA, tokenB));
-        totalSupply = pair.totalSupply();
-        (uint256 reserves0, uint256 reserves1, ) = pair.getReserves();
-        (reserveA, reserveB) = tokenA == pair.token0()
-            ? (reserves0, reserves1)
-            : (reserves1, reserves0);
+        pool = iFactory.createPair(tokenA, tokenB);
     }
 }
--- test/SushiSwapPool.test.ts
+++ test/SushiSwapPool.test.ts
@@ -1,16 +1,27 @@
-import { Signer } from "ethers";
+import { Signer, BigNumber } from "ethers";
 import { ethers } from "hardhat";
 import { expect } from "chai";
 import { SushiSwapPool } from "../artifacts/types/SushiSwapPool";
+import { IUniswapV2Factory } from "../artifacts/types/IUniswapV2Factory";
+import { IUniswapV2Pair } from "../artifacts/types/IUniswapV2Pair";
 import { UbiquityAlgorithmicDollarManager } from "../artifacts/types/UbiquityAlgorithmicDollarManager";
+import { UbiquityGovernance } from "../artifacts/types/UbiquityGovernance";
+import { UbiquityAlgorithmicDollar } from "../artifacts/types/UbiquityAlgorithmicDollar";
 
-const tokenA = "0x956f47f50a910163d8bf957cf5846d573e7f87ca";
-const tokenB = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2";
+const tokenA = "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48";
+const tokenB = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2";
+const factory = "0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f";
+const firstPair = "0xB4e16d0168e52d35CaCD2c6185b44281Ec28C9Dc";
+const nbPairs = 31941;
 
 describe("SushiSwapPool", () => {
   let admin: Signer;
   let manager: UbiquityAlgorithmicDollarManager;
   let sushi: SushiSwapPool;
+  let factoryContract: IUniswapV2Factory;
+  let onePairContract: IUniswapV2Pair;
+  let uAD: UbiquityAlgorithmicDollar;
+  let uGOV: UbiquityGovernance;
 
   beforeEach(async () => {
     [admin] = await ethers.getSigners();
@@ -21,19 +32,84 @@ describe("SushiSwapPool", () => {
       await admin.getAddress()
     )) as UbiquityAlgorithmicDollarManager;
 
+    factoryContract = (await ethers.getContractAt(
+      "IUniswapV2Factory",
+      factory
+    )) as IUniswapV2Factory;
+    onePairContract = (await ethers.getContractAt(
+      "IUniswapV2Pair",
+      firstPair
+    )) as IUniswapV2Pair;
+
+    const UAD = await ethers.getContractFactory("UbiquityAlgorithmicDollar");
+    uAD = (await UAD.deploy(manager.address)) as UbiquityAlgorithmicDollar;
+
+    const UGOV = await ethers.getContractFactory("UbiquityGovernance");
+    uGOV = (await UGOV.deploy(manager.address)) as UbiquityGovernance;
+
     const Sushi = await ethers.getContractFactory("SushiSwapPool");
-    sushi = (await Sushi.deploy(manager.address)) as SushiSwapPool;
+    sushi = (await Sushi.deploy(
+      manager.address,
+      uAD.address,
+      uGOV.address
+    )) as SushiSwapPool;
   });
-  describe("It", () => {
-    it("should work", async () => {
-      const [reserveA, reserveB, totalSupply] = await sushi.pairInfo(
-        tokenA,
-        tokenB
-      );
-      console.log(reserveA.toString());
-      console.log(reserveB.toString());
-      console.log(totalSupply.toString());
-      expect(true);
+
+  describe("UniSwap Factory", () => {
+    it(`should get ${nbPairs} pairs`, async () => {
+      const allPairsLength: BigNumber = await factoryContract.allPairsLength();
+
+      expect(allPairsLength).to.be.equal(nbPairs);
+    });
+    it("should get first pair 0xB4e...", async () => {
+      const pair = await factoryContract.allPairs(0);
+
+      expect(pair).to.be.equal(firstPair);
+    });
+  });
+
+  describe("UniSwap Pair", () => {
+    it("should get factory address from first pair", async () => {
+      const pairFactoryAddress = await onePairContract.factory();
+
+      expect(pairFactoryAddress).to.be.equal(factory);
+    });
+    it("should get tokens, reserves of first pair", async () => {
+      const token0 = await onePairContract.token0();
+      const token1 = await onePairContract.token1();
+      const [reserve0, reserve1] = await onePairContract.getReserves();
+
+      expect(token0).to.be.equal(tokenA);
+      expect(token1).to.be.equal(tokenB);
+      expect(reserve0).to.be.equal("144065947714472");
+      expect(reserve1).to.be.equal("75512578800914566328215");
+    });
+  });
+
+  describe("SushiSwap", () => {
+    it("should create pool", async () => {
+      const pool = await sushi.pool();
+
+      const allPairsLength: BigNumber = await factoryContract.allPairsLength();
+
+      const poolContract: IUniswapV2Pair = (await ethers.getContractAt(
+        "IUniswapV2Pair",
+        pool
+      )) as IUniswapV2Pair;
+
+      const token0 = await poolContract.token0();
+      const token1 = await poolContract.token1();
+      const [reserve0, reserve1] = await poolContract.getReserves();
+
+      if (token0 === uAD.address) {
+        expect(token0).to.be.equal(uAD.address);
+        expect(token1).to.be.equal(uGOV.address);
+      } else {
+        expect(token0).to.be.equal(uGOV.address);
+        expect(token1).to.be.equal(uAD.address);
+      }
+      expect(reserve0).to.be.equal("0");
+      expect(reserve1).to.be.equal("0");
     });
   });
 });
