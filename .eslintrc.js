c870de48657c9194bf173d595b600ae744e47de8
アレクサンダー.eth <git@.com>
2021-05-16 21:51:59 -0500

    eslint should work with nested projects

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,30 @@
-module.exports = {
-  root: true,
-  extends: [
-    "airbnb-typescript/base",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "plugin:prettier/recommended",
-    "prettier",
-  ],
-  parserOptions: {
-    project: "./tsconfig.eslint.json",
-  },
-  ignorePatterns: ["**/artifacts", "**/coverage"],
-  rules: {
-    // helps code clarity
-    "@typescript-eslint/comma-dangle": "off",
-    // we prefer double quotes and backticks
-    "@typescript-eslint/quotes": "off",
-    // we use it for scripts
-    "no-console": "off",
-    // we use it for tests
-    "func-names": "off",
-    // we use it for tests
-    "import/no-extraneous-dependencies": "off",
-    // we use it for tests
-    "@typescript-eslint/no-unused-expressions": "off",
-  },
+export default {
+    root: true,
+    extends: [
+        "airbnb-typescript/base",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@typescript-eslint/recommended-requiring-type-checking",
+        "plugin:prettier/recommended",
+        "prettier",
+    ],
+    parserOptions: {
+        project: "./tsconfig.eslint.json",
+        tsconfigRootDir: __dirname,
+        sourceType: "module",
+    },
+    ignorePatterns: ["**/artifacts", "**/coverage"],
+    rules: {
+        // helps code clarity
+        "@typescript-eslint/comma-dangle": "off",
+        // we prefer double quotes and backticks
+        "@typescript-eslint/quotes": "off",
+        // we use it for scripts
+        "no-console": "off",
+        // we use it for tests
+        "func-names": "off",
+        // we use it for tests
+        "import/no-extraneous-dependencies": "off",
+        // we use it for tests
+        "@typescript-eslint/no-unused-expressions": "off",
+    },
 };
